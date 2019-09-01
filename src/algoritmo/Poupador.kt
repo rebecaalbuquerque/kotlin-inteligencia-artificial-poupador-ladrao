dac1f22b37feca24e747b29a8bcd8333d26b12d2
Rebeca <rebeca.albuquerque96@gmail.com>
2019-09-01 13:53:42 -0300

    Iniciando implementação da função sucessor

--- src/algoritmo/Poupador.kt
+++ src/algoritmo/Poupador.kt
@@ -1,32 +1,37 @@
 package algoritmo
 
+import algoritmo.data.Estado
+import algoritmo.data.funcaoSucessor
 import algoritmo.extension.toEstado
 import java.util.*
 
-class Poupador: ProgramaPoupador() {
+class Poupador : ProgramaPoupador() {
 
-    var count = 0
+    private lateinit var estadoAtual: Estado
 
     override fun acao(): Int {
-        count++
+        estadoAtual = sensor.toEstado()
 
-        if(count == 2) {
-            count = 0
-        } else {
-            println("Visao identificacao    : " + Arrays.toString(sensor.visaoIdentificacao))
-            println("A,biente olfato ladrao : " + Arrays.toString(sensor.ambienteOlfatoLadrao))
-            println("Quantidade moedas      : ${sensor.numeroDeMoedas}")
-            println("Quantidade moedas banco: ${sensor.numeroDeMoedasBanco}")
-            println("Numero jogas imnune    : ${sensor.numeroJogadasImunes}")
-            println("Posicao                : ${sensor.posicao}")
+        val acoes = estadoAtual.funcaoSucessor()
+        val acao = acoes.random()
 
-            println("------------------------------------")
+        println("Visao identificacao    : " + Arrays.toString(sensor.visaoIdentificacao))
+        println("Ambiente olfato ladrao : " + Arrays.toString(sensor.ambienteOlfatoLadrao))
+        println("Quantidade moedas      : ${sensor.numeroDeMoedas}")
+        println("Quantidade moedas banco: ${sensor.numeroDeMoedasBanco}")
+        println("Numero jogas imnune    : ${sensor.numeroJogadasImunes}")
+        println("Posicao                : ${sensor.posicao}")
+        println("Acoes: $acoes")
+        println("Acao selecionada: $acao")
 
-        }
+        println("------------------------------------\n")
 
-        val estadoAtual = sensor.toEstado()
+        //return (Math.random() * 5).toInt()
 
-        return (Math.random() * 5).toInt()
+        if (acoes.isEmpty())
+            return 0
+
+        return acao.value
     }
 
 }
--- src/algoritmo/enums/Acao.kt
+++ src/algoritmo/enums/Acao.kt
@@ -1,11 +1,17 @@
 package algoritmo.enums
 
-enum class Acao(val valor: Int) {
+enum class Acao(val value: Int) {
 
     FICAR_PARADO(0),
     MOVER_CIMA(1),
     MOVER_BAIXO(2),
     MOVER_DIREITA(3),
-    MOVER_ESQUERDA(4)
+    MOVER_ESQUERDA(4);
+
+    companion object {
+        fun getByValue(i: Int): Acao {
+            return values().find { it.value == i } ?: FICAR_PARADO
+        }
+    }
 
 }
