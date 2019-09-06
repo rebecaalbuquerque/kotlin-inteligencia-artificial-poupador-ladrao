44e89b0932619cfd8f3982eda02687c830845d19
Rebeca <rebeca.albuquerque96@gmail.com>
2019-09-06 20:55:04 -0300

    Adicionando custo da pastilha do poder no calculo do custo da ação

--- src/algoritmo/Poupador.kt
+++ src/algoritmo/Poupador.kt
@@ -2,9 +2,8 @@ package algoritmo
 
 import algoritmo.data.Estado
 import algoritmo.enums.Acao
-import algoritmo.enums.PercepcaoVisao.MOEDA
+import algoritmo.enums.PercepcaoVisao.*
 import algoritmo.extension.*
-import java.util.*
 
 class Poupador : ProgramaPoupador() {
 
@@ -12,21 +11,6 @@ class Poupador : ProgramaPoupador() {
 
     override fun acao(): Int {
         estadoAtual = sensor.toEstado()
-
-        val acoes = estadoAtual.funcaoSucessor()
-        val acao = acoes.random()
-
-        println("Visao identificacao    : " + Arrays.toString(sensor.visaoIdentificacao))
-        println("Ambiente olfato ladrao : " + Arrays.toString(sensor.ambienteOlfatoLadrao))
-        println("Quantidade moedas      : ${sensor.numeroDeMoedas}")
-        println("Quantidade moedas banco: ${sensor.numeroDeMoedasBanco}")
-        println("Numero jogas imnune    : ${sensor.numeroJogadasImunes}")
-        println("Posicao                : ${sensor.posicao}")
-        println("Acoes: $acoes")
-        println("Acao selecionada: $acao")
-
-        println("------------------------------------\n")
-
         return acaoBaseadaNoCusto().value
     }
 
@@ -41,24 +25,16 @@ class Poupador : ProgramaPoupador() {
         estadoAtual.funcaoSucessor().forEach { mAcao ->
 
             when (mAcao) {
-                Acao.FICAR_PARADO -> { }
-
-                Acao.MOVER_CIMA -> {
-                    // Custo apenas na busca das moedas
-                    map[Acao.MOVER_CIMA] = estadoAtual.visaoPoupadorCima().filter { it == MOEDA.value }.size * MOEDA.custo
+                Acao.FICAR_PARADO -> {
                 }
 
-                Acao.MOVER_BAIXO -> {
-                    map[Acao.MOVER_BAIXO] = estadoAtual.visaoPoupadorBaixo().filter { it == MOEDA.value }.size * MOEDA.custo
-                }
+                Acao.MOVER_CIMA -> map[Acao.MOVER_CIMA] = calcularCusto(estadoAtual.visaoPoupadorCima())
 
-                Acao.MOVER_DIREITA -> {
-                    map[Acao.MOVER_DIREITA] = estadoAtual.visaoPoupadorDireita().filter { it == MOEDA.value }.size * MOEDA.custo
-                }
+                Acao.MOVER_BAIXO -> map[Acao.MOVER_BAIXO] = calcularCusto(estadoAtual.visaoPoupadorBaixo())
 
-                Acao.MOVER_ESQUERDA -> {
-                    map[Acao.MOVER_ESQUERDA] = estadoAtual.visaoPoupadorEsquerda().filter { it == MOEDA.value }.size * MOEDA.custo
-                }
+                Acao.MOVER_DIREITA -> map[Acao.MOVER_DIREITA] = calcularCusto(estadoAtual.visaoPoupadorDireita())
+
+                Acao.MOVER_ESQUERDA -> map[Acao.MOVER_ESQUERDA] = calcularCusto(estadoAtual.visaoPoupadorEsquerda())
             }
 
         }
@@ -72,7 +48,7 @@ class Poupador : ProgramaPoupador() {
         return custoMinimo?.let {
 
             // Se existir apenas uma ação com custo mínimo, faz essa ação
-            if(custoMinimoRepetido.size == 1) {
+            if (custoMinimoRepetido.size == 1) {
                 it.key
             } else {
                 // Se existir mais de uma ação com o mesmo custo mínimo
@@ -85,6 +61,17 @@ class Poupador : ProgramaPoupador() {
             Acao.FICAR_PARADO
         }
 
+    }
+
+    private fun calcularCusto(percepcao: IntArray): Int {
+        val custoMoeda = percepcao.filter { it == MOEDA.value }.size * MOEDA.custo
+        val custoBanco = if (estadoAtual.numeroMoedas > 10) percepcao.filter { it == BANCO.value }.size * BANCO.custo else 0
+        val custoPastilha = if (estadoAtual.numeroMoedas > 10 && estadoAtual.numeroJogadasImunes < 15)
+            percepcao.filter { it == PASTILHA_PODER.value }.size * PASTILHA_PODER.custo
+        else
+            0
+
+        return custoMoeda + custoBanco + custoPastilha
     }
 
 }
--- src/algoritmo/enums/PercepcaoVisao.kt
+++ src/algoritmo/enums/PercepcaoVisao.kt
@@ -8,9 +8,9 @@ enum class PercepcaoVisao(val value: Int, val custo: Int) {
     FORA_DO_AMBIENTE(-1, -1), // Colocar custo correto
     CELULA_VAZIA(0, -1), // Colocar custo correto
     PARECE(1, -1), // Colocar custo correto
-    BANCO(3, -1), // Colocar custo correto
+    BANCO(3, -1000), // Colocar custo correto
     MOEDA(4, -800),
-    PASTILHA_PODER(5, -1), // Colocar custo correto
+    PASTILHA_PODER(5, -900), // Colocar custo correto
     POUPADOR(100, -1), // Colocar custo correto
     LADRAO(200, -1) // Colocar custo correto
 
