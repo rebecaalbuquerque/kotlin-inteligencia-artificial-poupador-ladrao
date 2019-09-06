27a46d6867d6e2dad8b1e5b6b5489c7ba03dd57d
Rebeca <rebeca.albuquerque96@gmail.com>
2019-09-06 19:32:24 -0300

    Colocando mais lógica no calculo do custo da tomada de decisão baseada nas moedas e outras melhorias

--- src/algoritmo/Poupador.kt
+++ src/algoritmo/Poupador.kt
@@ -2,7 +2,7 @@ package algoritmo
 
 import algoritmo.data.Estado
 import algoritmo.enums.Acao
-import algoritmo.enums.PercepcaoVisao
+import algoritmo.enums.PercepcaoVisao.MOEDA
 import algoritmo.extension.*
 import java.util.*
 
@@ -27,51 +27,63 @@ class Poupador : ProgramaPoupador() {
 
         println("------------------------------------\n")
 
-        //return (Math.random() * 5).toInt()
-        //acaoBaseadoEmCusto()
-
-        if (acoes.isEmpty())
-            return 0
-
-        //return acao.value
-        return acaoBaseadoEmCusto().value
+        return acaoBaseadaNoCusto().value
     }
 
-    private fun acaoBaseadoEmCusto(): Acao {
+    /**
+     * Essa função gera um HashMap<Acao, Int> que consta as possíveis ações que o poupador pode realizar na jogada atual
+     * e seus respectivos custos. A ação que será tomada é a ação que possuir o menor custo
+     * */
+    private fun acaoBaseadaNoCusto(): Acao {
         val map = mutableMapOf<Acao, Int>()
 
-        // Calculando escolha das ações baseando-se apenas nas moedas (por enquanto)
+        // Calculando escolha das possíveis ações baseando-se apenas nas moedas (por enquanto)
         estadoAtual.funcaoSucessor().forEach { mAcao ->
 
             when (mAcao) {
                 Acao.FICAR_PARADO -> { }
 
                 Acao.MOVER_CIMA -> {
-                    val custoMoedas = estadoAtual.visaoPoupadorCima().filter { it == PercepcaoVisao.MOEDA.value }.size * (-800)
-                    map[Acao.MOVER_CIMA] = custoMoedas
+                    // Custo apenas na busca das moedas
+                    map[Acao.MOVER_CIMA] = estadoAtual.visaoPoupadorCima().filter { it == MOEDA.value }.size * MOEDA.custo
                 }
 
                 Acao.MOVER_BAIXO -> {
-                    val custoMoedas = estadoAtual.visaoPoupadorBaixo().filter { it == PercepcaoVisao.MOEDA.value }.size * (-800)
-                    map[Acao.MOVER_BAIXO] = custoMoedas
+                    map[Acao.MOVER_BAIXO] = estadoAtual.visaoPoupadorBaixo().filter { it == MOEDA.value }.size * MOEDA.custo
                 }
 
                 Acao.MOVER_DIREITA -> {
-                    val custoMoedas = estadoAtual.visaoPoupadorDireita().filter { it == PercepcaoVisao.MOEDA.value }.size * (-800)
-                    map[Acao.MOVER_DIREITA] = custoMoedas
+                    map[Acao.MOVER_DIREITA] = estadoAtual.visaoPoupadorDireita().filter { it == MOEDA.value }.size * MOEDA.custo
                 }
 
                 Acao.MOVER_ESQUERDA -> {
-                    val custoMoedas = estadoAtual.visaoPoupadorEsquerda().filter { it == PercepcaoVisao.MOEDA.value }.size * (-800)
-                    map[Acao.MOVER_ESQUERDA] = custoMoedas
+                    map[Acao.MOVER_ESQUERDA] = estadoAtual.visaoPoupadorEsquerda().filter { it == MOEDA.value }.size * MOEDA.custo
                 }
             }
 
         }
 
-        println()
+        // Descobrindo a ação que possui o menor custo
+        val custoMinimo: Map.Entry<Acao, Int>? = map.minBy { it.value }
 
-        return map.maxBy { it.value }?.key ?: Acao.FICAR_PARADO
+        // Descobrindo se tem mais de uma ação com o mesmo custo mínimo
+        val custoMinimoRepetido = map.filter { it.value == custoMinimo?.value }
+
+        return custoMinimo?.let {
+
+            // Se existir apenas uma ação com custo mínimo, faz essa ação
+            if(custoMinimoRepetido.size == 1) {
+                it.key
+            } else {
+                // Se existir mais de uma ação com o mesmo custo mínimo
+                custoMinimoRepetido.keys.random()
+            }
+
+        } ?: kotlin.run {
+            // Se o map de ações sucessores possíveis estiver vazio, então é porque a única coisa que o poupador pode
+            // fazer é ficar parado
+            Acao.FICAR_PARADO
+        }
 
     }
 
---
+++ src/algoritmo/enums/PercepcaoOlfato.kt
@@ -0,0 +1,14 @@
+package algoritmo.enums
+
+enum class PercepcaoOlfato(val value: Int, val custo: Int) {
+
+    // TODO: colocar valores corretos para o custo
+
+    SEM_CHEIRO(0, -1),
+    CHEIRO_1_UNIDADE_ATRAS(1, -1),
+    CHEIRO_2_UNIDADES_ATRAS(2, -1),
+    CHEIRO_3_UNIDADES_ATRAS(3, -1),
+    CHEIRO_4_UNIDADES_ATRAS(4, -1),
+    CHEIRO_5_UNIDADES_ATRAS(1, -1)
+
+}
